5ef90f59744c5a095aa5451e3c6a90edacc442ef
Uy Du Tran <uy-du.tran@epitech.eu>
2019-05-18 14:52:15 +0200

    Fixing the uwsgi.ini

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
 app.secret_key = 'jose'
+
 api = Api(app)
 
-
+@app.before_first_request
+def create_table():
+    db.create_all()
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
